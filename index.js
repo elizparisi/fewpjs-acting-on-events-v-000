59dbb9c74cbfcdef0862c48a94bbc945f0f19140
Liz P <parisielizabeth@gmail.com>
2020-05-04 22:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Your code here
 
 document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
+    dodger.style.left = `${left - 1}px`;
+  }
+});
+
+document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
     let rightNumbers = dodger.style.right.replace("px", "");
     let right = parseInt(rightNumbers, 10);
